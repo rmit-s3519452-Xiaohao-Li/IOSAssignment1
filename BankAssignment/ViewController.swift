16686e6cf1ba7bbb77eb40564089b717b78d7295
rmit-s3519452-Xiaohao-Li <Danny@xiaohaodemacbook.local>
2016-04-04 10:37:21 +1000

    Initial Commit

---
+++ BankAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankAssignment
+//
+//  Created by Xiaohao Li on 4/04/2016.
+//  Copyright © 2016 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
